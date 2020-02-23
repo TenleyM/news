16df0750a23cd0d2e78ef177c255a1ecd5d4928d
TenleyM <tenley.malmquist@gmail.com>
2020-02-23 21:29:22 +0000

    almost done

--- app.rb
+++ app.rb
@@ -7,12 +7,26 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "221adcc1033da8944da11b88bbe93ace"
+# url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2078680eea174645b906064f9ae8d13d"
+# news = HTTParty.get(url).parsed_response.to_hash
+
 
 get "/" do
+  view "location"
   # show a view that asks for the location
 end
 
 get "/news" do
-  # do everything else
+    @location = params["location_field"]
+    @location = @location.capitalize
+
+    @results = Geocoder.search(@location)
+    @lat_long = @results.first.coordinates
+    @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
+ 
+    @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2078680eea174645b906064f9ae8d13d"
+    @news = HTTParty.get(@url).parsed_response.to_hash
+
+    view "news"
 end
